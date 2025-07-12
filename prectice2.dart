4b94ce6529aac63388f128d3d630e18f1cde64d5
Arif khan <arif0852147@gmail.com>
2025-07-12 20:11:31 +0500

    userinput

--- prectice2.dart
+++ prectice2.dart
@@ -1,4 +1,6 @@
 void main() {
+  ///comment
+  /// this is a pi value
   const pi = 2;
   print('ali $pi');
 }
---
+++ userinput.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  print('Enter number');
+  String? name = stdin.readLineSync();
+  print('Enter Name is ${name}');
+}
